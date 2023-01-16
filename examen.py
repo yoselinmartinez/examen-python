51a6a87cb9e64a912fa8c44744e49aec22206fe4
Yoslink <kymargo02@gmail.com>
2023-01-16 16:35:42 -0600

    Hicimos el segundo ejercicio del examen
    Ejercicio de validacion de numeros

--- examen.py
+++ examen.py
@@ -1,5 +1,17 @@
-#Declara una variable con el nombre Beto y despues imprime un mensaje que diga "Hola Beto"
 
-nombre = "Beto"
+
+nombre = "Juan"
 
 print("Hola " +nombre)
+
+#Con un if ver si 20 es mayor a 15, imprimir verdadero si es verdad e imprimir falso en caso contrario 
+
+numero1 = 20
+numero2 = 15
+
+if numero1 > numero2:
+    print("Verdadero")
+else:
+    print("Falso")
+
+
